6541edaa59405206cb98f779b650d9e56a609fea
Domi <Dominik.janser@mail.com>
2023-02-24 20:30:31 +0100

    stuff

--- src/app/menu.ts
+++ src/app/menu.ts
@@ -1,33 +1,39 @@
 export let menu_products = [{
-    name: "test2",
-    img: "../../assets/Food/01_dish.png",
-    price: 202,
-    description: "bad stuff",
+    name: "Cheesecake",
+    img: "../../assets/Food/22_cheesecake.png",
+    price: 9.90,
+    description: "Good cheesecake, maybe even the best!",
     availability: true
 },
 {
-    name: "test",
-    img: "../../assets/Food/01_dish.png",
+    name: "Apple Pie",
+    img: "../../assets/Food/05_apple_pie.png",
     price: 20,
-    description: "good stuff yum yum",
+    description: "Worlds best apple pie, they say never available!",
     availability: false
 },{
-    name: "test",
-    img: "../../assets/Food/01_dish.png",
-    price: 20,
-    description: "good stuff yum yum",
+    name: "Bread",
+    img: "../../assets/Food/07_bread.png",
+    price: 4.5,
+    description: "Very good apetizer!",
     availability: true
 },{
-    name: "test",
-    img: "../../assets/Food/01_dish.png",
-    price: 20,
-    description: "good stuff yum yum",
+    name: "Burger",
+    img: "../../assets/Food/15_burger.png",
+    price: 11.90,
+    description: "Very good burger, cruelty free pixels!",
     availability: true
 },{
-    name: "test",
-    img: "../../assets/Food/01_dish.png",
-    price: 20,
-    description: "good stuff yum yum",
+    name: "Burrito",
+    img: "../../assets/Food/18_burrito.png",
+    price: 9.90,
+    description: "Very tasty, much wow",
+    availability: true
+},{
+    name: "Cookies",
+    img: "../../assets/Food/28_cookies.png",
+    price: 3.20,
+    description: "gBefore you leave, take some cookies",
     availability: true
 }];
 
