a1e7fe6801e2e7fd169ca5a92c47717b8bf5e7ca
Domi <Dominik.janser@mail.com>
2023-02-24 10:10:03 +0100

    added pictures, created routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { OrderComponent } from './order/order.component';
+import { AboutComponent } from './about/about.component';
+import { DetailsComponent } from './details/details.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:"", component: HomeComponent
+},{
+  path:"menu", component: MenuComponent
+},{
+  path:"order", component: OrderComponent
+},{
+  path:"about", component: AboutComponent
+},{
+  path:":productID", component: DetailsComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { MenuComponent } from './menu/menu.component';
+import { DetailsComponent } from './details/details.component';
+import { OrderComponent } from './order/order.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    FooterComponent,
+    AboutComponent,
+    MenuComponent,
+    DetailsComponent,
+    OrderComponent
   ],
   imports: [
     BrowserModule,
